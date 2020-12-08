a45ac8ab9d6b5f64ef9b20f4d8b422a9e89baa79
Billy Campbell <wcampbell91@gmail.com>
2020-12-07 21:08:02 -0600

    created files for viewsets

---
+++ rareapi/views/auth.py
@@ -0,0 +1,7 @@
+import json
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.views.decorators.csrf import csrf_exempt
+from rareapi.models import Gamer
