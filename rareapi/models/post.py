34630c58eb8c39c0f51c1d019f698f5496d29011
Joshua Medlen <joshua246@gmail.com>
2020-12-08 19:15:30 -0600

    added user to post model

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.conf import settings
 from django.db.models.fields import related
 
 class Post(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE)
     title = models.CharField(max_length=75)
     content = models.CharField(max_length=150)
     category = models.ForeignKey("Category",
