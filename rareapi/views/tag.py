a1625aa0dd8193f0e42a235c006c85cf851e10ea
Jim Browning <62916291+jamalbrowning@users.noreply.github.com>
2020-12-08 18:51:09 -0600

    Update tag.py

--- rareapi/views/tag.py
+++ rareapi/views/tag.py
@@ -3,4 +3,4 @@ from django.views.generic.base import View
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from rareapi.models import GameType
+
