4d41b2ded756133d9754e9110b7e6c10acadefca
Matt Logan <mlogan5212@gmail.com>
2020-12-14 20:40:08 -0600

    update comment list view, model

--- rareapi/models/comments.py
+++ rareapi/models/comments.py
@@ -3,8 +3,8 @@ from django.db.models import CASCADE
 
 
 class Comment(models.Model):
-    post = models.ForeignKey("Post", on_delete=CASCADE, related_name="comments", related_query_name="comment")
-    author = models.ForeignKey("RareUser", on_delete=CASCADE, related_name="comments", related_query_name="comment")
+    post = models.ForeignKey("Post", on_delete=CASCADE, related_name="posts", related_query_name="posts", null = False, blank = True)
+    author = models.ForeignKey("RareUser", on_delete=CASCADE, related_name="authors", related_query_name="author")
     content = models.TextField()
     subject = models.CharField(max_length=100)
     created_on = models.DateTimeField()
