41dd3fe7f506397f338dbfed71a70b21017008f9
Jim Browning <jamalebrowning@gmail.com>
2021-01-12 20:57:10 -0600

    fixed post view set

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -17,34 +17,46 @@ class PostViewSet(viewsets.ModelViewSet):
     filter_fields = ('category', 'category__id', 'user', 'user__id', 'tags__label' )
 
     def create(self, request):
-            rare_user = RareUser.objects.get(pk=request.data['user'])
-            format, imgstr = request.data['header_img_url'].split(';base64,')
-            ext = format.split('/')[-1]
-            image_data = ContentFile(base64.b64decode(imgstr), name=f'{uuid.uuid4()}.{ext}')
-            post = Post()
-            post.title = request.data['title']
-            post.publication_date = request.data['publication_date']
-            post.header_img_url = image_data
-            post.content = request.data['content']
-            post.user = rare_user
-            category = Category.objects.get(pk=request.data['category'])
-            post.category = category
-            try:
-                post.save()
-                post.tags.set(request.data["tags"])
-                post.save()
-                serializer = PostSerializer(post, context={'request': request})
-                return Response(serializer.data)
-            except ValidationError as ex:
-                return Response({'reason': ex.message}, status=status.HTTP_400_BAD_REQUEST)
+        rare_user = RareUser.objects.get(pk=request.data['user'])
+        format, imgstr = request.data['header_img_url'].split(';base64,')
+        ext = format.split('/')[-1]
+        image_data = ContentFile(base64.b64decode(imgstr), name=f'{uuid.uuid4()}.{ext}')
+        post = Post()
+        post.title = request.data['title']
+        post.publication_date = request.data['publication_date']
+        post.header_img_url = image_data
+        post.content = request.data['content']
+        post.user = rare_user
+        category = Category.objects.get(pk=request.data['category'])
+        post.category = category
+        try:
+            post.save()
+            post.tags.set(request.data["tags"])
+            post.save()
+            serializer = PostSerializer(post, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+          return Response({'reason': ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
+   
     def get_queryset(self):
-      user_id =self.request.body.query_params.get('user_id', None)
-      follower = self.request.query_params.get('subscribed', None)
-      if user_id:
-        return self.queryset.filter(user_id=user_id)
-      elif follower:
-        subscriptions = Subscription.objects.filter(follower_id=follower)
-        return self.queryset.filter(user_id__in=subscriptions.value("author_id"))
-      else:
-        return self.queryset
+        user_id = self.request.query_params.get('user_id', None)
+        follower = self.request.query_params.get('subscribed', None)
+        if user_id:
+          return self.queryset.filter(user_id=user_id)
+        elif follower:
+          subscriptions = Subscription.objects.filter(follower_id=follower)
+          return self.queryset.filter(user_id__in=subscriptions.values("author_id"))
+        else:
+          return self.queryset
+
+ # def get_queryset(self):
+    #   user_id =self.request.body.query_params.get('user_id', None)
+    #   follower = self.request.query_params.get('subscribed', None)
+      # if user_id:
+      #   return self.queryset.filter(user_id=user_id)
+      # elif follower:
+      #   subscriptions = Subscription.objects.filter(follower_id=follower)
+      #   return self.queryset.filter(user_id__in=subscriptions.value("author_id"))
+      # else:
+      #   return self.queryset
